a20e74d28258afce8412c32b42ae8cd4cfe5f9df
Chris-Grg <chris.grg7@gmail.com>
2023-02-27 00:01:56 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
+import FactsSection from "./components/FactsSection";
+import FooterSection from "./components/FooterSection";
+import HeroSection from "./components/HeroSection";
+import Home from "./components/Home";
+import PortfolioSection from "./components/PortfolioSection";
+import ResumeSection from "./components/ResumeSection";
+import ServicesSection from "./components/ServicesSection";
+import SkillsSection from "./components/SkillsSection";
+import TestimonialsSection from "./components/TestimonialsSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import AboutSection from "./AboutSection";
+import FactsSection from "./FactsSection";
+import SkillsSection from "./SkillsSection";
+import ResumeSection from "./ResumeSection";
+import PortfolioSection from "./PortfolioSection";
+import ServicesSection from "./ServicesSection";
+import TestimonialsSection from "./TestimonialsSection";
+import ContactSection from "./ContactSection";
+import FooterSection from "./FooterSection";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <i className="bi bi-list mobile-nav-toggle d-lg-none"></i>
+        <HeroSection />
+        <main id="main">
+          {/* <!-- ======= About Section ======= --> */}
+          <AboutSection />
+          {/* <!-- ======= Facts Section ======= --> */}
+          <FactsSection />
+          {/* <!-- End Facts Section --> */}
+          {/* <!-- ======= Skills Section ======= --> */}
+          <SkillsSection />
+          {/* <!-- End Skills Section --> */}
+          {/* <!-- ======= Resume Section ======= --> */}
+          <ResumeSection />
+          {/* <!-- End Resume Section --> */}
+          {/* <!-- ======= Portfolio Section ======= --> */}
+          <PortfolioSection />
+          {/* <!-- End Portfolio Section --> */}
+          {/* <!-- ======= Services Section ======= --> */}
+          <ServicesSection />
+          {/* <!-- End Services Section --> */}
+          {/* <!-- ======= Testimonials Section ======= --> */}
+          <TestimonialsSection /> {/* <!-- End Testimonials Section --> */}
+          {/* <!-- ======= Contact Section ======= --> */}
+          <ContactSection />
+          {/* <!-- End Contact Section --> */}
+        </main>
+        {/* <!-- End #main --> */}
+
+        {/* <!-- ======= Footer ======= --> */}
+        <FooterSection />
+        {/* <!-- End Footer --> */}
+
+        {/* <div id="preloader"></div>
+        <a
+          href="#"
+          className="back-to-top d-flex align-items-center justify-content-center"
+        >
+          <i className="bi bi-arrow-up-short"></i>
+        </a> */}
+      </div>
+    </>
+  );
+}
+
+export default Home;
